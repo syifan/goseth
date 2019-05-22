2393596e32aef403a5e704f417249f3e3fa95f50
Yifan Sun <sunyifan112358@gmail.com>
2019-05-21 21:35:43 -0400

    deserialize some primitive types

--- deserializer.go
+++ deserializer.go
@@ -2,6 +2,7 @@ package seth
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
 	"io/ioutil"
 	"strconv"
@@ -39,17 +40,127 @@ func (d deserializerImpl) parseValue(
 	json.Unmarshal(typeNameBytes, &typeName)
 
 	switch typeName {
+	case "bool":
+		return d.parseBoolValue(m["value"])
 	case "int":
 		return d.parseIntValue(m["value"])
+	case "int8":
+		return d.parseInt8Value(m["value"])
+	case "int16":
+		return d.parseInt16Value(m["value"])
+	case "int32":
+		return d.parseInt32Value(m["value"])
+	case "int64":
+		return d.parseInt64Value(m["value"])
+	case "uint":
+		return d.parseUintValue(m["value"])
+	case "uint8":
+		return d.parseUint8Value(m["value"])
+	case "uint16":
+		return d.parseUint16Value(m["value"])
+	case "uint32":
+		return d.parseUint32Value(m["value"])
+	case "uint64":
+		return d.parseUint64Value(m["value"])
+
+	default:
+		return nil, errors.New("type " + typeName + "is not supported")
 	}
-	return nil, nil
+}
+
+func (d deserializerImpl) parseBoolValue(m *json.RawMessage) (bool, error) {
+	valueString := rawMsgMustConvertToString(m)
+	switch valueString {
+	case "true":
+		return true, nil
+	case "false":
+		return false, nil
+	default:
+		return false, errors.New("bool value can only be true or false")
+	}
 }
 
 func (d deserializerImpl) parseIntValue(m *json.RawMessage) (int, error) {
+	valueString := rawMsgMustConvertToString(m)
+	value := stringMustConvertToInt(valueString)
+	return int(value), nil
+}
+
+func (d deserializerImpl) parseInt8Value(m *json.RawMessage) (int8, error) {
+	valueString := rawMsgMustConvertToString(m)
+	value := stringMustConvertToInt(valueString)
+	return int8(value), nil
+}
+
+func (d deserializerImpl) parseInt16Value(m *json.RawMessage) (int16, error) {
+	valueString := rawMsgMustConvertToString(m)
+	value := stringMustConvertToInt(valueString)
+	return int16(value), nil
+}
+
+func (d deserializerImpl) parseInt32Value(m *json.RawMessage) (int32, error) {
+	valueString := rawMsgMustConvertToString(m)
+	value := stringMustConvertToInt(valueString)
+	return int32(value), nil
+}
+
+func (d deserializerImpl) parseInt64Value(m *json.RawMessage) (int64, error) {
+	valueString := rawMsgMustConvertToString(m)
+	value := stringMustConvertToInt(valueString)
+	return int64(value), nil
+}
+
+func (d deserializerImpl) parseUintValue(m *json.RawMessage) (uint, error) {
+	valueString := rawMsgMustConvertToString(m)
+	value := stringMustConvertToInt(valueString)
+	return uint(value), nil
+}
+
+func (d deserializerImpl) parseUint8Value(m *json.RawMessage) (uint8, error) {
+	valueString := rawMsgMustConvertToString(m)
+	value := stringMustConvertToInt(valueString)
+	return uint8(value), nil
+}
+
+func (d deserializerImpl) parseUint16Value(m *json.RawMessage) (uint16, error) {
+	valueString := rawMsgMustConvertToString(m)
+	value := stringMustConvertToInt(valueString)
+	return uint16(value), nil
+}
+
+func (d deserializerImpl) parseUint32Value(m *json.RawMessage) (uint32, error) {
+	valueString := rawMsgMustConvertToString(m)
+	value := stringMustConvertToInt(valueString)
+	return uint32(value), nil
+}
+
+func (d deserializerImpl) parseUint64Value(m *json.RawMessage) (uint64, error) {
+	valueString := rawMsgMustConvertToString(m)
+	value := stringMustConvertToInt(valueString)
+	return uint64(value), nil
+}
+
+func rawMsgMustConvertToString(m *json.RawMessage) string {
 	valueBytes, err := m.MarshalJSON()
 	if err != nil {
-		return 0, err
+		panic(err)
 	}
-	valueInt, err := strconv.ParseInt(string(valueBytes), 10, 32)
-	return int(valueInt), nil
+
+	return string(valueBytes)
+}
+
+func stringMustConvertToInt(s string) int64 {
+	valueInt, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return valueInt
+}
+
+func stringMustConvertToUint(s string) uint64 {
+	valueInt, err := strconv.ParseUint(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return valueInt
 }
--- deserializer_test.go
+++ deserializer_test.go
@@ -17,6 +17,17 @@ var _ = Describe("Deserializer", func() {
 		d = MakeDeserializer()
 	})
 
+	It("should deserilize bool", func() {
+		str := `{
+			"value": true,
+			"t": "bool"
+		}`
+
+		a, err := d.Deserialize(strings.NewReader(str))
+
+		Expect(err).To(BeNil())
+		Expect(a).To(BeTrue())
+	})
 	It("should deserilize int", func() {
 		str := `{
 			"value": 1,
@@ -27,5 +38,113 @@ var _ = Describe("Deserializer", func() {
 
 		Expect(err).To(BeNil())
 		Expect(a).To(Equal(1))
+	})
+
+	It("should deserilize int8", func() {
+		str := `{
+			"value": 1,
+			"t": "int8"
+		}`
+
+		a, err := d.Deserialize(strings.NewReader(str))
+
+		Expect(err).To(BeNil())
+		Expect(a).To(Equal(int8(1)))
+	})
+
+	It("should deserilize int16", func() {
+		str := `{
+			"value": 1,
+			"t": "int16"
+		}`
+
+		a, err := d.Deserialize(strings.NewReader(str))
+
+		Expect(err).To(BeNil())
+		Expect(a).To(Equal(int16(1)))
+	})
+
+	It("should deserilize int32", func() {
+		str := `{
+			"value": 1,
+			"t": "int32"
+		}`
+
+		a, err := d.Deserialize(strings.NewReader(str))
+
+		Expect(err).To(BeNil())
+		Expect(a).To(Equal(int32(1)))
+	})
+
+	It("should deserilize int64", func() {
+		str := `{
+			"value": 1,
+			"t": "int64"
+		}`
+
+		a, err := d.Deserialize(strings.NewReader(str))
+
+		Expect(err).To(BeNil())
+		Expect(a).To(Equal(int64(1)))
+	})
+
+	It("should deserilize uint", func() {
+		str := `{
+			"value": 1,
+			"t": "uint"
+		}`
+
+		a, err := d.Deserialize(strings.NewReader(str))
+
+		Expect(err).To(BeNil())
+		Expect(a).To(Equal(uint(1)))
+	})
+
+	It("should deserilize uint8", func() {
+		str := `{
+			"value": 1,
+			"t": "uint8"
+		}`
+
+		a, err := d.Deserialize(strings.NewReader(str))
+
+		Expect(err).To(BeNil())
+		Expect(a).To(Equal(uint8(1)))
+	})
+
+	It("should deserilize uint16", func() {
+		str := `{
+			"value": 1,
+			"t": "uint16"
+		}`
+
+		a, err := d.Deserialize(strings.NewReader(str))
+
+		Expect(err).To(BeNil())
+		Expect(a).To(Equal(uint16(1)))
+	})
+
+	It("should deserilize uint32", func() {
+		str := `{
+			"value": 1,
+			"t": "uint32"
+		}`
+
+		a, err := d.Deserialize(strings.NewReader(str))
+
+		Expect(err).To(BeNil())
+		Expect(a).To(Equal(uint32(1)))
+	})
+
+	It("should deserilize uint64", func() {
+		str := `{
+			"value": 1,
+			"t": "uint64"
+		}`
+
+		a, err := d.Deserialize(strings.NewReader(str))
+
+		Expect(err).To(BeNil())
+		Expect(a).To(Equal(uint64(1)))
 	})
 })
