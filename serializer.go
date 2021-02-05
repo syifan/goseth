f605809add80dec313db83979ee235cb514cb7fe
Yifan Sun <sunyifan112358@gmail.com>
2021-02-04 22:14:17 -0500

    Support serializing unsigned integers

--- serializer.go
+++ serializer.go
@@ -171,9 +171,12 @@ func (s *serializerImpl) serializeItem(
 	value := s.dict[id]
 	currDepth := s.idToDepthMapping[id]
 	switch value.Kind() {
-	case reflect.Int, reflect.Int16, reflect.Int32, reflect.Int64:
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 		fmt.Fprintf(writer, `{"v":%d,"t":"%s","k":%d}`,
 			value.Int(), s.typeString(value), value.Kind())
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		fmt.Fprintf(writer, `{"v":%d,"t":"%s","k":%d}`,
+			value.Uint(), s.typeString(value), value.Kind())
 	case reflect.Float32, reflect.Float64:
 		fmt.Fprintf(writer, `{"v":%f,"t":"%s","k":%d}`,
 			value.Float(), s.typeString(value), value.Kind())
