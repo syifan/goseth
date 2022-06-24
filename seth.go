34f6311637bb817c60768d35e147f50500616819
Yifan Sun <sunyifan112358@gmail.com>
2022-06-23 21:31:41 -0400

    Starting a full redesign

--- seth.go
+++ seth.go
@@ -1,13 +1,23 @@
+// Package goseth defines a serializer for Go data structures.
 package goseth
 
 import "io"
 
-// Serializer can serialize anything into a json format
+// Serializer can serialize anything into a json format.
 type Serializer interface {
-	Serialize(item interface{}, writer io.Writer) error
+	// SetRoot sets the item to be serialized.
+	SetRoot(item any)
+
+	// SetMaxDepth sets the maximum depth of the serialization. If the depth
+	// is exceeded, the serializer will stop serializing the item.
+	// SetMaxDepth(0) means no limit.
+	SetMaxDepth(depth int)
+
+	// Serialize serializes the item into the writer.
+	Serialize(writer io.Writer) error
 }
 
-// Deserializer can parse a serialized string back to an object
-type Deserializer interface {
-	Deserialize(reader io.Reader) (interface{}, error)
+// NewSerializer returns a new default serializer.
+func NewSerializer() Serializer {
+	return &serializer{}
 }
--- test/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/syifan/seth/test
-
-go 1.12
-
-require (
-	github.com/onsi/ginkgo v1.8.0
-	gitlab.com/akita/akita v1.3.2
-)
